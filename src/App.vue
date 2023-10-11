946200387367e08c4dfea54732505566cf36cd04
Ceci <cecitsai30218@gmail.com>
2023-10-11 14:50:29 +0800

    Initialization

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <header>
+    <div class="test">
+      <img alt="Vue logo" class="logo" :src="`./photo.JPG`" height=450 />
+    </div>
+
+    <div class="wrapper">
+      <HelloWorld msg="蔡涵伃 Han Yu, Tsai" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+    border-radius: 10%;
+    -moz-border-radius: 10%;
+   -webkit-border-radius: 10%;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+
+.test {
+  height: 85%;
+
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,70 @@
+<script setup>
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+import ToolingIcon from "./icons/IconTooling.vue";
+import EcosystemIcon from "./icons/IconEcosystem.vue";
+import CommunityIcon from "./icons/IconCommunity.vue";
+import SupportIcon from "./icons/IconSupport.vue";
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Hi~ 我是涵伃</template>
+    歡迎來到我的個人網站
+
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>專業技能</template>
+
+    Revit、Dynamo、Navisworks、Twinmotion、AutoCAD、Ms Project、QGIS、C++、Python、Vue.js
+
+    <br />
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>學歷</template>
+
+    大學 | 國立台北科技大學 ( 2020 – 2024/06 )
+    <br>
+    高中 | 國立中科實驗高中 ( 2016 – 2019/06 )
+
+
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>作品連結</template>
+
+
+
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>聯絡資料</template>
+
+    Email : cecitsai30218@gmail.com.tw
+    <br>
+    連絡電話 : 0905-361-691
+
+    
+  </WelcomeItem>
+</template>
